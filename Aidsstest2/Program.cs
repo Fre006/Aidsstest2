547a45925707228017f237351984e8820b00b8e9
Frederick Grønbech Kure Ertel <fre006@edu.zealand.dk>
2025-04-08 10:09:49 +0200

    dskmfd

--- Aidsstest2/Program.cs
+++ Aidsstest2/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> groceries = new()
+            {
+                "hvedemel", "Bagepulver", "Fint salt", "æg", "sukker", "vaniljesukker", "smeltet smør", "bananer", "mørk chokolade"
+
+            };
+            List<string> amounts = new()
+            {
+                "200g", "2 teskeer", "½ teske", "2", "150g", "1 teske", "125g", "2", "100g"
+            };
+            for(int i = 0; i < 9; i++)
+            {
+                Console.Write(amounts[i]);
+                Console.Write(" ");
+                Console.WriteLine(groceries[i]);
+            }
         }
     }
 }
